3b2c598fc98862f577b50b2b6b815a10b4775ca1
Arvind2698 <r.arvind007@gmail.com>
2020-06-16 18:33:17 +0530

    Version 1.0

---
+++ include/customerFeedback.php
@@ -0,0 +1,96 @@
+<section class="ftco-section testimony-section bg-dark">
+        <div class="container">
+            <div class="row justify-content-center pb-5 mb-3">
+                <div class="col-md-7 heading-section heading-section-white text-center ftco-animate">
+                    <span class="subheading">Testimonies</span>
+                    <h2>Happy Clients &amp; Feedbacks</h2>
+                </div>
+            </div>
+            <div class="row ftco-animate">
+                <div class="col-md-12">
+                    <div class="carousel-testimony owl-carousel ftco-owl">
+                        <div class="item">
+                            <div class="testimony-wrap py-4">
+                                <div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-quote-left"></span></div>
+                                <div class="text">
+                                    <p class="mb-4">
+                                        To render total quality eye care at affordable charges,with state-of-the-art technology and higly qualified staff.</p>
+                                    <div class="d-flex align-items-center">
+                                        <div class="user-img" style="background-image: url(images/persn_1.jpg)"></div>
+                                        <div class="pl-3">
+                                            <p class="name">xxxxxx</p>
+                                            <span class="position">Vijaywada Branch</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="testimony-wrap py-4">
+                                <div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-quote-left"></span></div>
+                                <div class="text">
+                                    <p class="mb-4">
+                                        One of the best hospital and equipment in Rajamundry, complete satisafaction based on how people react around the doctor, excellent help by staff. Doctor treatment is fabouls. Helped me a lot to improve my vision.</p>
+                                    <div class="d-flex align-items-center">
+                                        <div class="user-img" style="background-image: url(images/rupesh.jpeg)"></div>
+                                        <div class="pl-3">
+                                            <p class="name">Rupesh Nukala</p>
+                                            <span class="position">Rajamundry Branch</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="testimony-wrap py-4">
+                                <div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-quote-left"></span></div>
+                                <div class="text">
+                                    <p class="mb-4">
+                                        To render total quality eye care at affordable charges,with state-of-the-art technology and higly qualified staff.</p>
+                                    <div class="d-flex align-items-center">
+                                        <div class="user-img" style="background-image: url(images/rup.jpeg)"></div>
+                                        <div class="pl-3">
+                                            <p class="name">xxxxx</p>
+                                            <span class="position">Vijaywada branch</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="testimony-wrap py-4">
+                                <div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-quote-left"></span></div>
+                                <div class="text">
+                                    <p class="mb-4">
+                                        To render total quality eye care at affordable charges,with state-of-the-art technology and higly qualified staff.</p>
+                                    <div class="d-flex align-items-center">
+                                        <div class="user-img" style="background-image: url(images/person_1.jpg)"></div>
+                                        <div class="pl-3">
+                                            <p class="name">Roger Scott</p>
+                                            <span class="position">Marketing Manager</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="testimony-wrap py-4">
+                                <div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-quote-left"></span></div>
+                                <div class="text">
+                                    <p class="mb-4">
+                                        To render total quality eye care at affordable charges,with state-of-the-art technology and higly qualified staff.</p>
+                                    <div class="d-flex align-items-center">
+                                        <div class="user-img" style="background-image: url(images/person_2.jpg)"></div>
+                                        <div class="pl-3">
+                                            <p class="name">Roger Scott</p>
+                                            <span class="position">Marketing Manager</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
